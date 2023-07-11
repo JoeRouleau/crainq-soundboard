1bc3b539bfb8d9197755c1ba8e643a4b5beb4b73
JoeRouleau <57806884+JoeRouleau@users.noreply.github.com>
2023-07-11 14:39:50 -0400

    Download des vocaux

--- src/app/voice-message/voice-message.component.ts
+++ src/app/voice-message/voice-message.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import {Howl} from 'howler';
 import { Vocal } from '../interfaces/Vocal';
 import { SoundMode } from '../interfaces/SoundMode';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-voice-message',
@@ -13,6 +14,8 @@ export class VoiceMessageComponent {
   mode : SoundMode = SoundMode.Stop;
   iconName: string = "play_circle_outline";
   sound: Howl = {} as Howl;
+
+  constructor(private http: HttpClient) {}
 
   public click(mode: string) {
     if(mode === SoundMode.Stop) {
@@ -39,6 +42,20 @@ export class VoiceMessageComponent {
     this.mode = SoundMode.Stop;
     this.iconName = "play_circle_outline"
     this.sound.stop();
+  }
+
+  public download() {
+    const audioFileUrl = this.vocal.path;
+
+    this.http.get(audioFileUrl, { responseType: 'blob'}).subscribe(response => {
+      const downloadLink = document.createElement('a');
+      const blob = new Blob([response], {type: 'audio/wav'});
+      const url = URL.createObjectURL(blob);
+      downloadLink.href = url;
+      downloadLink.download = `${this.vocal.path}.wav`
+      downloadLink.click();
+      URL.revokeObjectURL(url);
+    })
   }
 
 }
